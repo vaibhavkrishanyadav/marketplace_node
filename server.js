51dea8f427b1d928981cd0c368594cfc177d36d3
vaibhavkrishanyadav <vaibhavkrishanyadav@gmail.com>
2023-12-14 23:24:48 +0530

    connected to db

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const connectDb = require('./config/dbConnection');
+const dotenv = require("dotenv").config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
