c6ce8d3ddb27ed9e5bc2bae2bec036af81ecaa6c
vaibhavkrishanyadav <vaibhavkrishanyadav@gmail.com>
2024-01-08 01:01:31 +0530

    fixed typo issue

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,4 @@
-const asyncHandler = require('express-async-Handler');
+const asyncHandler = require('express-async-handler');
 const Category = require('../models/categoryModel');
 
 //@desc Get all category
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,4 @@
-const asyncHandler = require('express-async-Handler');
+const asyncHandler = require('express-async-handler');
 const Product = require('../models/productModel');
 
 //@desc Get product
