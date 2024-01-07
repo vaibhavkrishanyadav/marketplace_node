cab6ae112f937034c3da60bae799722a16900658
vaibhavkrishanyadav <vaibhavkrishanyadav@gmail.com>
2024-01-08 00:53:38 +0530

    added product controller

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/category/product', require('./routes/productRoutes'));
 app.use('/api/category', require('./routes/categoryRoutes'));
 app.use(errorHandler);
 
